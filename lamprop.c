2ecd407f01ee7a59ba746ec5d7b37b8a67f9b547
Roland Smith <rsmith@xs4all.nl>
2025-08-03 20:17:33 +0200

    Define fatal macro.

--- lamprop.c
+++ lamprop.c
@@ -4,16 +4,22 @@
 // Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: MIT
 // Created: 2025-08-03T19:20:39+0200
-// Last modified: 2025-08-03T19:24:24+0200
+// Last modified: 2025-08-03T19:31:01+0200
 
 #include "logging.h"
 
 #include <stdint.h>
 #include <stdbool.h>
-#include <assert.h>
-#include <stdio.h>
+#include <stdio.h>  // for fprintf(3)
 #include <stdlib.h> // for abort(3)
 #include <string.h>
+
+#undef fatal
+#define fatal(...)                                                  \
+  fprintf(stderr, "FATAL ERROR %s, line %i: ", __FILE__, __LINE__); \
+  fprintf(stderr, __VA_ARGS__);                                     \
+  abort()
+
 
 int main(int argc, char *argv[])
 {
