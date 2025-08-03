089289c3d3c09016c975ede8c3e08028e3909ef2
Roland Smith <rsmith@xs4all.nl>
2025-08-03 19:19:28 +0200

    Import stringview.

---
+++ sv.c
@@ -0,0 +1,94 @@
+// file: sv.c
+// vim:fileencoding=utf-8:ft=c:tabstop=2
+//
+// Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
+// SPDX-License-Identifier: MIT
+// Created: 2025-04-07 22:53:56 +0200
+// Last modified: 2025-04-09T10:13:15+0200
+
+#include <assert.h>
+#include <string.h>
+
+#include "sv.h"
+
+bool sv8equals(Sv8 a, Sv8 b)
+{
+  return (a.len == b.len) && (!a.len || !memcmp(a.data, b.data, a.len));
+}
+
+Sv8 sv8lstrip(Sv8 s)
+{
+  while (s.len && *s.data<=' ') {
+    s.data++, s.len--;
+  }
+  return s;
+}
+
+Sv8 sv8rstrip(Sv8 s)
+{
+  while (s.len && s.data[s.len-1]<=' ') {
+    s.len--;
+  }
+  return s;
+}
+
+Sv8 sv8strip(Sv8 s)
+{
+  while (s.len && *s.data<=' ') {
+    s.data++, s.len--;
+  }
+  while (s.len && s.data[s.len-1]<=' ') {
+    s.len--;
+  }
+  return s;
+}
+
+ptrdiff_t sv8find(Sv8 s, Sv8 f)
+{
+  if (s.len == 0 || f.len == 0 || f.len > s.len) {
+    return -1;
+  }
+  ptrdiff_t maxoff = s.len - f.len;
+  for (ptrdiff_t off = 0; off <= maxoff ; off++) {
+    if (memcmp(s.data+off, f.data, f.len) == 0) {
+      return off;
+    }
+  }
+  return -1;
+}
+
+ptrdiff_t sv8count(Sv8 s, char f)
+{
+  ptrdiff_t rv = 0;
+  for (ptrdiff_t j = 0; j < s.len; j++) {
+    if (s.data[j] == f) {
+      rv++;
+    }
+  }
+  return rv;
+}
+
+Sv8 sv8span(char *beg, char *end)
+{
+  Sv8 r = {0};
+  r.data = beg;
+  r.len  = beg ? end-beg : 0;
+  return r;
+}
+
+Cut sv8cut(Sv8 s, char c)
+{
+  Cut r = {0};
+  if (s.len == 0) {
+    return r;
+  }
+  char *beg = s.data;
+  char *end = s.data + s.len;
+  char *cut = beg;
+  for (; cut<end && *cut!=c; cut++) {}
+  r.ok   = cut < end;
+  r.head = sv8span(beg, cut);
+  r.tail = sv8span(cut+r.ok, end);
+  return r;
+}
+
---
+++ sv.h
@@ -0,0 +1,62 @@
+// file: sv.h
+// vim:fileencoding=utf-8:ft=cpp:tabstop=2
+//
+// Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
+// SPDX-License-Identifier: MIT
+// Created: 2025-04-07 22:53:56 +0200
+// Last modified: 2025-04-09T10:13:50+0200
+
+#pragma once
+#include <stddef.h>  // ptrdiff_t
+#include <stdbool.h>  // bool
+
+#define SV8(s)    (Sv8){s, sizeof(s)-1}
+
+// Note that these strings do not allocate memory.
+// So they are *views* of a string.
+
+typedef struct {
+  char *data;
+  ptrdiff_t len;
+} Sv8;
+
+typedef struct {
+  Sv8 head;
+  Sv8 tail;
+  bool ok;
+} Cut;
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Test two strings for equality of length and content.
+extern bool sv8equals(Sv8 a, Sv8 b);
+
+// Remove whitespace from the left of a string.
+extern Sv8 sv8lstrip(Sv8 s);
+
+// Remove whitespace from the right of a string.
+extern Sv8 sv8rstrip(Sv8 s);
+
+// Remove whitespace from both ends of the string.
+extern Sv8 sv8strip(Sv8 s);
+
+// Return the offset of the string f in the string s.
+// If not found, return -1.
+extern ptrdiff_t sv8find(Sv8 s, Sv8 f);
+
+// Return the amount of occurrances of the character f in string s.
+extern ptrdiff_t sv8count(Sv8 s, char f);
+
+// Return the Sv8 that stretched from beg to end.
+extern Sv8 sv8span(char *beg, char *end);
+
+// Cut the string s at the character c.
+extern Cut sv8cut(Sv8 s, char c);
+
+
+#ifdef __cplusplus
+}
+#endif
