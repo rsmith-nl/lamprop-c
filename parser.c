6c4a4e945c468d52b13b43e8a2682e45a7de6fb2
Roland Smith <rsmith@xs4all.nl>
2025-08-04 01:34:59 +0200

    Begin parser.

---
+++ parser.c
@@ -0,0 +1,35 @@
+// file: parser.c
+// vim:fileencoding=utf-8:ft=c:tabstop=2
+//
+// Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
+// SPDX-License-Identifier: MIT
+// Created: 2025-08-04 00:11:34 +0200
+// Last modified: 2025-08-04T00:33:02+0200
+
+#include "arena.h"
+#include "stringview.h"
+#include "logging.h"
+
+//#include <stdint.h>
+//#include <stdbool.h>
+#include <stdio.h>  // for fopen
+//#include <stdlib.h>
+#include <sys/mman.h> // for mmap
+
+Sv8 read_file(char *path, Arena *permanent)
+{
+  Sv8 contents = {0};
+  FILE *inputfile = fopen(path, "r");
+  fseek(inputfile, 0L, SEEK_END);
+  ptrdiff_t size = ftell(inputfile);
+  rewind(inputfile);
+  contents.data = arena_new(permanent, char, size);
+  contents.len = size;
+  ptrdiff_t rv = fread(contents.data, sizeof(char), size, inputfile);
+  if (rv != size) {
+    fprintf(stderr,
+        "WARNING: file “%s” has size %td bytes, but only %td bytes read.\n",
+        path, size, rv);
+  }
+  return contents;
+}
