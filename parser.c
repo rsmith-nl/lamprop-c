f4c1fe8225d5974ff5ad3cb22168839bb1980326
Roland Smith <rsmith@xs4all.nl>
2025-08-06 21:52:24 +0200

    Fix parsing resin and fiber.

--- parser.c
+++ parser.c
@@ -4,7 +4,7 @@
 // Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: MIT
 // Created: 2025-08-04 00:11:34 +0200
-// Last modified: 2025-08-05T22:37:16+0200
+// Last modified: 2025-08-06T21:50:05+0200
 
 #include "arena.h"
 #include "stringview.h"
@@ -43,18 +43,102 @@ Resin parse_resin(Sv8 line)
 {
   Resin rv = {0};
   Sv8Cut cut = sv8lsplit(line);
-  // This function is only called when *line* starts with 'r:'.
+  // This function is only called when *line* starts with 'f:'.
   // So discard that.
-  cut = sv8lsplit(cut.tail);
-  //cut.head contains the Young's modulus.
+  // cut.tail now starts with the Young's modulus after whitespace.
+  Sv8Double E = sv8tod(cut.tail);
+  if (E.ok) {
+    rv.E = E.result;
+    rv.ok = true;
+    //debug("E = %g\n", E.result);
+  } else {
+    return rv; // empty
+  }
+  // E.tail now starts with the Poisson constant after whitespace.
+  Sv8Double ν = sv8tod(E.tail);
+  if (ν.ok) {
+    rv.ν = ν.result;
+    rv.ok = true;
+    //debug("ν = %g\n", ν.result);
+  } else {
+    rv.ok = false;
+    return rv;
+  }
+  // ν.tail now starts with the CTE after whitespace.
+  Sv8Double α = sv8tod(ν.tail);
+  if (α.ok) {
+    rv.α = α.result;
+    rv.ok = true;
+    //debug("α = %g\n", α.result);
+  } else {
+    rv.ok = false;
+    return rv;
+  }
+  // α.tail now starts with the density after whitespace.
+  Sv8Double ρ = sv8tod(α.tail);
+  if (ρ.ok) {
+    rv.ρ = ρ.result;
+    rv.ok = true;
+    //debug("ρ = %g\n", ρ.result);
+  } else {
+    rv.ok = false;
+    return rv;
+  }
+  // ρ.tail now starts with the name after whitespace.
+  rv.name = sv8strip(ρ.tail);
+  //debug("rv.name.len = %ld\n", rv.name.len);
+  rv.ok = true;
   return rv;
 }
 
 Fiber parse_fiber(Sv8 line)
 {
-  // This function is only called when *line* starts with 'f:'.
-  // So we don't have to check that.
   Fiber rv = {0};
+  Sv8Cut cut = sv8lsplit(line);
+  // This function is only called when *line* starts with 'f:'.
+  // So discard that.
+  // cut.tail now starts with the Young's modulus after whitespace.
+  Sv8Double E1 = sv8tod(cut.tail);
+  if (E1.ok) {
+    rv.E1 = E1.result;
+    rv.ok = true;
+    //debug("E1 = %g\n", E1.result);
+  } else {
+    return rv; // empty
+  }
+  // E1.tail now starts with the Poisson constant after whitespace.
+  Sv8Double ν12 = sv8tod(E1.tail);
+  if (ν12.ok) {
+    rv.ν12 = ν12.result;
+    rv.ok = true;
+    //debug("ν12 = %g\n", ν12.result);
+  } else {
+    rv.ok = false;
+    return rv;
+  }
+  // ν12.tail now starts with the CTE after whitespace.
+  Sv8Double α1 = sv8tod(ν12.tail);
+  if (α1.ok) {
+    rv.α1 = α1.result;
+    rv.ok = true;
+    //debug("α1 = %g\n", α1.result);
+  } else {
+    rv.ok = false;
+    return rv;
+  }
+  // α1.tail now starts with the density after whitespace.
+  Sv8Double ρ = sv8tod(α1.tail);
+  if (ρ.ok) {
+    rv.ρ = ρ.result;
+    rv.ok = true;
+    //debug("ρ = %g\n", ρ.result);
+  } else {
+    rv.ok = false;
+    return rv;
+  }
+  // ρ.tail now starts with the name after whitespace.
+  rv.name = sv8strip(ρ.tail);
+  rv.ok = true;
   return rv;
 }
 
