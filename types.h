6b43d937eaa9ac20d8aa2ab473fce97914349013
Roland Smith <rsmith@xs4all.nl>
2025-08-06 19:46:53 +0200

    Use double instead of float.

--- types.h
+++ types.h
@@ -4,64 +4,70 @@
 // Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: MIT
 // Created: 2025-08-03T19:31:50+0200
-// Last modified: 2025-08-05T21:43:28+0200
+// Last modified: 2025-08-06T19:45:51+0200
 
 #pragma once
 
 #include "arena.h"
 #include "stringview.h"
 
+#include <stdbool.h>
+
 typedef struct {
-  float E;
-  float ν;
-  float α;
-  float ρ;
+  double E;
+  double ν;
+  double α;
+  double ρ;
   Sv8 name;
+  bool ok;
 } Resin;
 
 typedef struct {
-  float E1;
-  float ν12;
-  float α1;
-  float ρ;
+  double E1;
+  double ν12;
+  double α1;
+  double ρ;
   Sv8 name;
+  bool ok;
 } Fiber;
 
 typedef struct {
   Fiber f;
   Resin r;
-  float fiber_weight;
-  float angle;
-  float vf;
-  float thickness;
-  float resin_weight;
-  float E1, E2, E3;
-  float G12, G13, G23;
-  float ν12, ν13, ν23;
-  float αx, αy, αxy;
-  float Q̅11, Q̅12, Q̅16, Q̅22, Q̅26, Q̅66, Q̅s44, Q̅s55, Q̅s45;
-  float ρ;
-  float C[6][6];
+  double fiber_weight;
+  double angle;
+  double vf;
+  double thickness;
+  double resin_weight;
+  double E1, E2, E3;
+  double G12, G13, G23;
+  double ν12, ν13, ν23;
+  double αx, αy, αxy;
+  double Q̅11, Q̅12, Q̅16, Q̅22, Q̅26, Q̅66, Q̅s44, Q̅s55, Q̅s45;
+  double ρ;
+  double C[6][6];
+  bool ok;
 } Lamina;
 
 typedef struct {
   Sv8 name;
   int32_t nlayers;
   Lamina *layers;
-  float thickness;
-  float fiber_weight, resin_weight;
-  float ρ;
-  float vf, wf;
-  float ABD[6][6], abd[6][6];
-  float H[2][2], h[2][2];
-  float Ex, Ey, Ez;
-  float Gxy, Gyz, Gxz;
-  float νxy, νyx;
-  float αx, αy;
-  float C[6][6], S[6][6];
-  float tEx, tEy, tEz;
-  float tGxy, tGyz, tGxz;
-  float tνxy, tνyx, tνyz;
+  double thickness;
+  double fiber_weight, resin_weight;
+  double ρ;
+  double vf, wf;
+  double ABD[6][6], abd[6][6];
+  double H[2][2], h[2][2];
+  double Ex, Ey, Ez;
+  double Gxy, Gyz, Gxz;
+  double νxy, νyx;
+  double αx, αy;
+  double C[6][6], S[6][6];
+  double tEx, tEy, tEz;
+  double tGxy, tGyz, tGxz;
+  double tνxy, tνyx, tνyz;
+  bool ok;
 } Laminate;
 
 #ifdef __cplusplus
