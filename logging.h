b27018c07ce75b7cba8d3ff32d5b5a65b2d6b747
Roland Smith <rsmith@xs4all.nl>
2025-08-11 18:32:06 +0200

    Update logging with application name.

--- logging.h
+++ logging.h
@@ -4,20 +4,22 @@
 // Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: MIT
 // Created: 2025-08-04 00:49:24 +0200
-// Last modified: 2025-08-08T14:22:13+0200
+// Last modified: 2025-08-11T18:29:41+0200
 
 #pragma once
+
+#define NM "#lamprop-c "
 
 #undef error
 #ifndef NDEBUG
 #define error(...)                                            \
-  fprintf(stderr, "ERROR %s, line %i: ", __FILE__, __LINE__); \
+  fprintf(stderr, NM"ERROR %s, line %i: ", __FILE__, __LINE__); \
   fprintf(stderr, __VA_ARGS__);                               \
   fprintf(stderr, "\n");                                      \
   abort()
 #else
 #define error(...)                \
-  fprintf(stderr, "ERROR: ");     \
+  fprintf(stderr, NM"ERROR: ");     \
   fprintf(stderr, __VA_ARGS__);   \
   fprintf(stderr, "\n");          \
   abort()
@@ -26,7 +28,7 @@
 #undef debug
 #ifndef NDEBUG
 #define debug(...)                                            \
-  fprintf(stderr, "DEBUG %s, line %i: ", __FILE__, __LINE__); \
+  fprintf(stderr, NM"DEBUG %s, line %i: ", __FILE__, __LINE__); \
   fprintf(stderr, __VA_ARGS__);                               \
   fprintf(stderr, "\n")
 #else
@@ -36,12 +38,12 @@
 #undef warn
 #ifndef NDEBUG
 #define warn(...)                                            \
-  fprintf(stderr, "WARNING %s, line %i: ", __FILE__, __LINE__); \
+  fprintf(stderr, NM"WARNING %s, line %i: ", __FILE__, __LINE__); \
   fprintf(stderr, __VA_ARGS__); \
   fprintf(stderr, "\n")
 #else
 #define warn(...) \
-  fprintf(stderr, "WARNING: "); \
+  fprintf(stderr, NM"WARNING: "); \
   fprintf(stderr, __VA_ARGS__); \
   fprintf(stderr, "\n")
 #endif  // NDEBUG
@@ -49,7 +51,7 @@
 #undef info
 #ifndef NDEBUG
 #define info(...)                                            \
-  fprintf(stderr, "INFO %s, line %i: ", __FILE__, __LINE__); \
+  fprintf(stderr, NM"INFO %s, line %i: ", __FILE__, __LINE__); \
   fprintf(stderr, __VA_ARGS__); \
   fprintf(stderr, "\n")
 #else
