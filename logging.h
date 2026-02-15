1a55f140795f5929623156fe80ba7ac7ab45a70c
Roland Smith <rsmith@xs4all.nl>
2026-02-16 00:42:36 +0100

    warning without debug.

--- logging.h
+++ logging.h
@@ -4,7 +4,7 @@
 // Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: MIT
 // Created: 2025-08-04 00:49:24 +0200
-// Last modified: 2026-01-19T23:12:40+0100
+// Last modified: 2026-02-15T12:18:52+0100
 
 #pragma once
 
@@ -64,5 +64,9 @@
   fprintf(stderr, "\n"); \
   fflush(stderr)
 #else
-#define info(...) (void)0
+#define warn(...) \
+  fprintf(stderr, NM"INFO: "); \
+  fprintf(stderr, __VA_ARGS__); \
+  fprintf(stderr, "\n"); \
+  fflush(stderr)
 #endif  // NDEBUG
