c38e8bb1f21503373335306748939f0fa08eab7f
Roland Smith <rsmith@xs4all.nl>
2026-04-11 20:01:55 +0200

    Update stringbuf library.

--- sbuf.c
+++ sbuf.c
@@ -1,20 +1,22 @@
-// file: buffer.c
+// file: sbuf.c
 // vim:fileencoding=utf-8:ft=c:tabstop=2
 // This is free and unencumbered software released into the public domain.
 //
 // Author: R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: Unlicense
 // Created: 2025-08-28 23:49:02 +0200
-// Last modified: 2026-02-22T11:28:54+0100
+// Last modified: 2026-04-11T20:02:45+0200
 
 #include "sbuf.h"
 #include <assert.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
 
-void sbuf_append(Sbuf *buf, const char *str, const ptrdiff_t len)
+void sbuf_append(Sbuf *buf, char *str, ptrdiff_t len)
 {
   assert(buf != 0);
   assert(str != 0);
@@ -32,17 +34,128 @@ void sbuf_append(Sbuf *buf, const char *str, const ptrdiff_t len)
   }
 }
 
-inline void sbuf_appends(Sbuf *buf, const char *str)
+void sbuf_appendc(Sbuf *buf, char c)
 {
   assert(buf != 0);
   if (buf->error == true) {
     return;
   }
   ptrdiff_t remaining = SBUF_SIZE - buf->used - 1;
-  sbuf_append(buf, str, strnlen(str, remaining));
+  if (remaining >= 1) {
+    buf->data[buf->used++] = c;
+    buf->error = false;
+  } else {
+    buf->error = true;
+  }
 }
 
-void sbuf_printf(Sbuf *buf, const char *fmt, ...)
+inline void sbuf_appends(Sbuf *buf, char *str)
+{
+  sbuf_append(buf, str, strlen(str));
+}
+
+extern void sbuf_appendi(Sbuf *buf, int64_t i)
+{
+  assert(buf != 0);
+  if (buf->error == true) {
+    return;
+  }
+#define BUFLENI 21
+#define ORD0 48
+  int64_t work = 0;
+  bool negative = false;
+  if (i < 0) {
+    work = -i;
+    negative = true;
+  } else {
+    work = i;
+  }
+  char tmpbuf[BUFLENI] = {0};
+  int index = sizeof(tmpbuf) - 1;
+  do {
+    ldiv_t result = ldiv(work, 10);
+    tmpbuf[index--] = ORD0 + result.rem;
+    work = result.quot;
+  } while (work > 0 && index > 1);
+  if (negative) {
+    assert(index > 0);
+    tmpbuf[index--] = '-';
+  }
+  sbuf_appends(buf, tmpbuf + index + 1);
+}
+
+static double frexp10(double arg, int *exp)
+{
+  *exp = (arg == 0) ? 0 : (int)floor(log10(fabs(arg)));
+  return arg * pow(10, -(*exp));
+}
+
+extern void sbuf_appendd(Sbuf *buf, double f)
+{
+#define BUFLEND 30
+#define EXPLEN (BUFLEND-4-1)
+  assert(buf != 0);
+  if (buf->error != false) {
+    return;
+  }
+  int exp;
+  double mantissa = frexp10(f, &exp);
+  double am = fabs(mantissa);
+  char tbuf[BUFLEND] = {0};
+  int bufused = 0;
+  if (signbit(f)) {
+    tbuf[bufused++] = '-';
+  }
+  if (isinf(f)) {
+    char inf[4] = {0xE2, 0x88, 0x9E, 0}; // UTF-8 for infinity symbol.
+    sbuf_appends(buf, inf);
+    return;
+  } else if (mantissa == 0 && exp == 0) {
+    tbuf[bufused++] = '0';
+    sbuf_appends(buf, tbuf);
+    return;
+  } else if (isnan(f)) {
+    sbuf_appends(buf, "NaN");
+    return;
+  }
+  int decimal = (int)floor(am);
+  tbuf[bufused++] = ORD0 + decimal;
+  am = (am - decimal) * 10.0;
+  if (buf->decsep != 0) {
+    tbuf[bufused++] = buf->decsep;
+  } else {
+    tbuf[bufused++] = '.';
+  }
+  int loopexp = 0;
+  do {
+    decimal = (int)floor(am);
+    tbuf[bufused++] = ORD0 + decimal;
+    am = (am - decimal) * 10.0;
+    frexp10(am, &loopexp);
+    if (decimal == 9 && am > 9.5) {
+      break;
+    }
+  } while (am > 0 && loopexp == 0 && bufused < EXPLEN);
+  tbuf[bufused++] = 'e';
+  if (exp < 0) {
+    tbuf[bufused++] = '-';
+    exp = -exp;
+  } else {
+    tbuf[bufused++] = '+';
+  }
+  if (exp > 100) {
+    tbuf[bufused++] = ORD0 + 1;
+    exp -= 100;
+  }
+  if (exp > 10) {
+    tbuf[bufused++] = ORD0 + exp / 10;
+    exp -= 10;
+  }
+  tbuf[bufused++] = ORD0 + exp;
+  sbuf_appends(buf, tbuf);
+}
+
+void sbuf_printf(Sbuf *buf, char *fmt, ...)
 {
   assert(buf != 0);
   assert(fmt != 0);
--- sbuf.h
+++ sbuf.h
@@ -5,7 +5,7 @@
 // Author: R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: Unlicense
 // Created: 2025-08-28 23:49:02 +0200
-// Last modified: 2026-02-22T11:28:46+0100
+// Last modified: 2026-04-11T14:18:24+0200
 
 // Simple string buffer.
 // Mostly conceived for assembling strings.
@@ -16,6 +16,7 @@
 
 #include <stddef.h>  // for ptrdiff_t
 #include <stdbool.h> // for bool
+#include <stdint.h>
 #include <stdio.h>   // for FILE*
 #include <limits.h>  // for PATH_MAX
 
@@ -24,6 +25,7 @@
 typedef struct {
   ptrdiff_t used;
   bool error;  // use “error” instead of “ok” so a zerod-out Sbuf is valid.
+  char decsep; // decimal separator for sbuf_appendd. If 0, then "." is used.
   char data[SBUF_SIZE];
 } Sbuf;
 
@@ -35,13 +37,22 @@ extern "C" {
 // All appends immediately return if “error” is “true”.
 
 // Appends at most “len” bytes to “buf” from “str”.
-extern void sbuf_append(Sbuf *buf, const char *str, const ptrdiff_t len);
+extern void sbuf_append(Sbuf *buf, char *str, ptrdiff_t len);
+
+// Appends a character.
+extern void sbuf_appendc(Sbuf *buf, char c);
 
 // Appends null-terminated strings “str” to “buf”.
-extern void sbuf_appends(Sbuf *buf, const char *str);
+extern void sbuf_appends(Sbuf *buf, char *str);
 
-// Appends using *printf.
-extern void sbuf_printf(Sbuf *buf, const char *fmt, ...);
+// Format and append an integer number to buf.
+extern void sbuf_appendi(Sbuf *buf, int64_t i);
+
+// Format and append a double to buf.
+extern void sbuf_appendd(Sbuf *buf, double f);
+
+// Append using snprintf.
+extern void sbuf_printf(Sbuf *buf, char *fmt, ...);
 
 // Returns how much space remains in the buffer “buf”.
 extern ptrdiff_t sbuf_remaining(Sbuf *buf);
