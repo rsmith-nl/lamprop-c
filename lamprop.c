013d3ea6a9bf2c5cfb48ccf2b14b02cb7f22cb77
Roland Smith <rsmith@xs4all.nl>
2026-05-25 02:29:48 +0200

    Accept and use output file argument.

--- lamprop.c
+++ lamprop.c
@@ -4,12 +4,14 @@
 // Copyright © 2025 R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: MIT
 // Created: 2025-08-03T19:20:39+0200
-// Last modified: 2026-02-19T00:24:56+0100
+// Last modified: 2026-05-25T02:20:10+0200
 
 #include "core.h"
 #include "logging.h"
 #include "parser.h"
 #include "setup.h"
+
+#include <stdlib.h>
 
 #ifdef _WIN32
 #include <io.h> // for _setmode
@@ -20,11 +22,11 @@ extern int __stdcall SetConsoleOutputCP(unsigned int);
 #define PASZ 33554432
 
 // text.c
-extern void text_out(Laminate *pl, bool eng, bool mat, bool fea);
+extern void text_out(Laminate *pl, Options opts);
 // latex.c
-extern void latex_out(Laminate *pl, bool eng, bool mat, bool fea);
+extern void latex_out(Laminate *pl, Options opts);
 // html.c
-extern void html_out(Laminate *pl, bool eng, bool mat, bool fea);
+extern void html_out(Laminate *pl, Options opts);
 
 int main(int argc, char *argv[])
 {
@@ -35,54 +37,65 @@ int main(int argc, char *argv[])
 #endif
   Options opt = setup(argc, argv);
   debug("starting lamprop...");
-  while (opt.argc > 0) {
-    debug("opt.argc = %d", opt.argc);
-    debug("opt.argv[0] = %s", opt.argv[0]);
-    // General allocation arena. Stores file contents.
-    // This is also used as the storage for strings.
-    Arena permanent = arena_create(PASZ);
-    ParseResult file_result = parse_file(opt.argv[0], &permanent);
-    for (int32_t j = 0; j < file_result.tu; j++) {
-      Laminate *pl = file_result.laminates + j;
-      if (!finish_laminate(pl)) {
-        continue; // skip the laminate.
-      }
-      switch (opt.output) {
-        case TEXT:
-          text_out(pl, opt.eng, opt.matrix, opt.fea);
-          break;
-        case LATEX:
-          latex_out(pl, opt.eng, opt.matrix, opt.fea);
-          break;
-        case HTML:
-          html_out(pl, opt.eng, opt.matrix, opt.fea);
-          break;
-      }
-#ifndef NDEBUG
-      if (pl->magic == LMNT) {
-        debug("- laminate %d is a valid laminate", j + 1);
-        int32_t valid_la = 0;
-        for (int32_t k = 0; k < pl->nlayers; k++) {
-          Lamina *pa = pl->layers + k;
-          if (pa->magic == LAYR) {
-            valid_la++;
-          }
-        }
-        debug("  it has %d/%d valid lamina", valid_la, pl->nlayers);
-      }
-#endif
-    }
-    info("permanent arena, %d of %d bytes used", permanent.current_offset, PASZ);
-    info("%d of %d resins used", file_result.ru, file_result.r);
-    info("%d of %d fibers used", file_result.fu, file_result.f);
-    info("%d of %d laminates used", file_result.tu, file_result.t);
-    info("%d of %d lamina used", file_result.lu, file_result.l);
-    // Clean up
-    arena_destroy(&permanent);
-    // Advance
-    opt.argv++;
-    opt.argc--;
+  debug("opt.argc = %d", opt.argc);
+  debug("opt.argv[0] = %s", opt.argv[0]);
+  if (opt.argc == 0) {
+    error("no input filename given");
+    return EXIT_FAILURE;
   }
+  if (opt.output_filename != 0) {
+    opt.outfile = fopen(opt.output_filename, "w");
+    if (opt.outfile == 0) {
+      return EXIT_FAILURE;
+    }
+  } else {
+    // No filename given, use stdout.
+    opt.outfile = stdout;
+  }
+  // General allocation arena. Stores file contents.
+  // This is also used as the storage for strings.
+  Arena permanent = arena_create(PASZ);
+  ParseResult file_result = parse_file(opt.argv[0], &permanent);
+  for (int32_t j = 0; j < file_result.tu; j++) {
+    Laminate *pl = file_result.laminates + j;
+    if (!finish_laminate(pl)) {
+      continue; // skip the laminate.
+    }
+    switch (opt.output) {
+      case TEXT:
+        text_out(pl, opt);
+        break;
+      case LATEX:
+        latex_out(pl, opt);
+        break;
+      case HTML:
+        html_out(pl, opt);
+        break;
+    }
+#ifndef NDEBUG
+    if (pl->magic == LMNT) {
+      debug("- laminate %d is a valid laminate", j + 1);
+      int32_t valid_la = 0;
+      for (int32_t k = 0; k < pl->nlayers; k++) {
+        Lamina *pa = pl->layers + k;
+        if (pa->magic == LAYR) {
+          valid_la++;
+        }
+      }
+      debug("  it has %d/%d valid lamina", valid_la, pl->nlayers);
+    }
+#endif
+  }
+  if (opt.output_filename != 0) {
+    fclose(opt.outfile);
+  }
+  info("permanent arena, %d of %d bytes used", permanent.current_offset, PASZ);
+  info("%d of %d resins used", file_result.ru, file_result.r);
+  info("%d of %d fibers used", file_result.fu, file_result.f);
+  info("%d of %d laminates used", file_result.tu, file_result.t);
+  info("%d of %d lamina used", file_result.lu, file_result.l);
+  // Clean up
+  arena_destroy(&permanent);
   debug("ending lamprop normally...");
-  return 0;
+  return EXIT_SUCCESS;
 }
