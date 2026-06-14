540a475b12f37025183474e8ec0fef5d79a02bc2
Roland Smith <rsmith@xs4all.nl>
2026-06-14 11:28:21 +0200

    Quiet-down make-release.sh.

--- make-release.sh
+++ make-release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Script to make a new release.
+# Script to make and commit a new release.
 
 # Get variables
 BASENAME=lamprop-c
@@ -8,22 +8,22 @@ RELEASE=`git tag -l |tail -n 1 | tr -d '\n'`
 # Get the current date.
 DATE=` date +%Y.%m.%d`
 # Get the abbreviated commit hash of the latest commit.
-COMMIT=`git log -n 1 --pretty='%h'|tr -d '\n'`
+COMMIT=`git rev-parse --short HEAD|tr -d '\n'`
 
 # Remove duplicate tag, if any
 git tag -d ${DATE}
 
-echo "Updating version.h"
+# Updating version.h
 echo "#define VERSION \""${DATE}"\"" >version.h
 echo "#define LONG_VERSION \""${BASENAME}" version "${DATE}" (commit "${COMMIT}")\"" >>version.h
 
-echo "Updating Makefile"
+# Updating Makefile
 sed -i '' -e "s/^RELDATE.*/RELDATE=${DATE}/" Makefile
 
-if git status -s -- version.h | grep 'M' >/dev/null; then
-    echo "version.h modified; committing it."
+# Committing modified files.
+if git status -s -- version.h Makefile| grep 'M' >/dev/null; then
     git add version.h Makefile
-    git commit -m "Updated version.h"
+    git commit -m "Update version."
 fi
 
 # Tag release.
